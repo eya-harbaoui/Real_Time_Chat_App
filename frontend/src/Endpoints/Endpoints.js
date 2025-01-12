1b8ae3851f4d67f16ad56efd258ef86e2e1c3cd2
eya-harbaoui <eyaharbaoui651@gmail.com>
2025-01-12 23:16:34 +0100

    change endpoinst

--- frontend/src/Endpoints/Endpoints.js
+++ frontend/src/Endpoints/Endpoints.js
@@ -1,4 +1,4 @@
 export const BACKEND_URL = "https://talkio-prod.onrender.com/";
-export const AUTH_URL = "http://localhost:5000/api/auth";
-export const MESSAGES_URL = "http://localhost:5000/api/message";
-export const USERS_URL = "http://localhost:5000/api/users";
+export const AUTH_URL = "https://talkio-prod.onrender.com/api/auth";
+export const MESSAGES_URL = "https://talkio-prod.onrender.com/api/message";
+export const USERS_URL = "https://talkio-prod.onrender.com/api/users";
