6885f96e17b023d7a111b479507e9be9e55969b4
eya-harbaoui <eyaharbaoui651@gmail.com>
2025-01-12 23:09:11 +0100

    change the backend url by the new app url after deploy

--- frontend/src/Endpoints/Endpoints.js
+++ frontend/src/Endpoints/Endpoints.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://localhost:5000";
+export const BACKEND_URL = "https://talkio-prod.onrender.com/";
 export const AUTH_URL = "http://localhost:5000/api/auth";
 export const MESSAGES_URL = "http://localhost:5000/api/message";
 export const USERS_URL = "http://localhost:5000/api/users";
