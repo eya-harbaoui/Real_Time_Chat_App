e9ed92a4d6401b3a8a9f897dd0540a70f3ebe8f9
eya-harbaoui <eyaharbaoui651@gmail.com>
2025-01-12 23:04:31 +0100

    changes in server.js to fix deployment issue

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import path from "path";
 //files imports
 import { app, server } from "./socket/socket.js";
@@ -12,20 +11,16 @@ import usersRouters from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 //variables
-const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 //config call
 dotenv.config();
+const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 5000;
 
 //middlewares (app.use to apply middlewares globally : should be declared before routes def)
 app.use(express.json()); // parse the incoming requests with JSON payloads(from req.body)
 app.use(cookieParser());
 // Alternatively, you can specify allowed origins:
-
-app.get("/", (req, res) => {
-  //root route
-  res.send("Server is ready !");
-});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
